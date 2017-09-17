b3494903903062a5b88974f0816b59512b1827a2
Alessandro <Alessandro@ALEMASK-PC>
2017-09-17 17:49:07 +0200

    Primo scheletro UDPSender e UserConfiguration

---
+++ TestApplication/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class UserConfiguration
+    {
+
+        private static bool _PrivacyFlag; //TRUE: Pubblic, FALSE: Private
+        private static string _Username;
+        private static string _ImgPath;
+        private static bool mustsaveflag;
+   //     public readonly object _UserDatalocker = new object(); 
+   //TODO: Risolvere il problema della gestione del locker
+
+        /*TRUE=ci sono nuovi dati da salvare. Viene svegliato il thread a bassa priorità per salvare la configurazione. Se in fase di terminazione del programma ci sono ancora dati
+         * da salvare, si forza un salvataggio
+         FALSE=i dati contenuti in userconfiguration corrispondono a quelli contenuti nel file di configurazione su disco
+         */
+            
+        public bool PrivacyFlag { get { return _PrivacyFlag; }   set { _PrivacyFlag = value; } } 
+        public string Username  { get { return _Username;    }   set { _Username = value;    } }
+        public string ImgPath   { get { return _ImgPath;     }   set { _ImgPath = value;     } }
+
+        public UserConfiguration() { }
+
+        public UserConfiguration(bool Flag, string user, string img)
+        {
+            _PrivacyFlag = Flag;
+            _Username = user;
+            _ImgPath = img;
+            mustsaveflag = false;
+        }
+
+        public void DumpConfiguration() {  //TODO: Gestione eccezioni e path come parametro -->possibili eccezioni sono date da path errati o permessi mancanti
+            string json = JsonConvert.SerializeObject(this);
+            System.IO.File.WriteAllText(@"C:\Users\Alessandro\Desktop\config.json", json); 
+        }
+
+        public void LoadConfiguration() //TODO: Gestione eccezioni e path come parametro
+        {
+                String JSONstring = File.ReadAllText(@"C:\Users\Alessandro\Desktop\config.json"); 
+                UserConfiguration user = JsonConvert.DeserializeObject<UserConfiguration>(JSONstring);
+        }
+
+
+
+        private static void SaveConfiguration()
+        {
+            
+        }
+
+
+
+        public void StartConfigurationRoutine()
+        {
+
+            Thread savingroutine = new Thread(SaveConfiguration); //TODO: Trovare soluzione alternativa
+
+            savingroutine.Name = "savingroutine";
+
+            savingroutine.Priority = ThreadPriority.Lowest;
+
+        }
+    }
+}
---
+++ TestApplication/UDPSender.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    class UDPSender
+    {
+
+        private static UdpClient udpclient;
+        private static IPAddress groupAddress;
+        private IPEndPoint Clientdest;
+        private UserConfiguration user;
+
+        private static char[] tGreetings = { 'H', 'e', 'l', 'l', 'o', ' ', 'O', 'r', 'i', 'g', 'i', 'n', 'a', 't', 'o', 'r', '!' };
+        
+        public UDPSender(string MulticastAddress)
+
+        {
+            user = new UserConfiguration();
+            udpclient = new UdpClient();
+            groupAddress = IPAddress.Parse(MulticastAddress);
+            udpclient.JoinMulticastGroup(groupAddress);
+            Clientdest = new IPEndPoint(groupAddress, 2000);
+        }
+
+        private static Byte[] GetByteArray(Char[] ChArray)
+
+        {
+            Byte[] Ret = new Byte[ChArray.Length];
+            for (int i = 0; i < ChArray.Length; i++)
+                Ret[i] = (Byte)ChArray[i];
+            return Ret;
+        }
+
+        public void Start()
+        {
+            while (true)
+
+            {
+                Thread.Sleep(3000);
+              //  lock (user._UserDatalocker) ;
+                if (user.PrivacyFlag)
+                    udpclient.Send(GetByteArray(tGreetings), tGreetings.Length, Clientdest);//TODO: Specificare dati da passare
+                else break;
+
+            }
+        }
+
+
+
+    }
+
+}
+

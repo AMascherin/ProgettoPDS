d5ff78643c1312c3d0aa51a4c57d319c8fdb46b5
FedericoFerrero <federicoferrero076@gmail.com>
2017-09-22 16:31:30 +0200

    Add files via upload

---
+++ codiceHub.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Hub //TODO: gestire la responsività con gli eventi(tcp client, interfaccia grafica, cambio della flag della privacy
+    {
+        private List<NetworkUser> userlist;
+        private UserConfiguration uc;
+
+        public Hub() {
+
+            userlist = new List<NetworkUser>();
+            uc = new UserConfiguration();
+        
+        }
+
+        public void Initialize() { //fa la parte di startup
+
+            uc.LoadConfiguration();
+            //TODO: creare un thread per UDPsender, UDPlistener
+            this.TCPServerStartup();
+        }
+
+        private void TCPServerStartup()
+        {
+            throw new NotImplementedException();
+        
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
